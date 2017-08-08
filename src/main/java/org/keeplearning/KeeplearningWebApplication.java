ff81cbb3441b479433931b001c4450711699621f
Lonely Ranger <gorbenkoav@gmail.com>
2017-08-08 16:23:49 +0300

    Add default project settings made by Spring Boot Initializr

---
+++ src/main/java/org/keeplearning/KeeplearningWebApplication.java
@@ -0,0 +1,12 @@
+package org.keeplearning;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KeeplearningWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KeeplearningWebApplication.class, args);
+	}
+}
---
+++ src/test/java/org/keeplearning/KeeplearningWebApplicationTests.java
@@ -0,0 +1,16 @@
+package org.keeplearning;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class KeeplearningWebApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
